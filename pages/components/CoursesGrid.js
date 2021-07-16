d0fbe9c60a6de08140004829c2e6a0c5e50ff3a8
Praj964 <prajwify@gmail.com>
2021-07-16 12:57:47 +0530

    dotmap commented out

--- pages/components/CoursesGrid.js
+++ pages/components/CoursesGrid.js
@@ -3,7 +3,7 @@ import React from "react";
 const CoursesGrid = ({ data }) => {
   return (
     <div className="grid grid-rows-1 gap-4 lg:gap-6">
-      {data.map((p) => {
+      {/* data.map((p) => {
         return (
           <div
             className="flex flex-col items-start justify-center w-full h-full gap-3 p-4 rounded-md shadow-sm dark:shadow-inner dark:bg-gray-900 dark:hover:bg-gray-800 transiton dark:text-white bg-gray-100 hover:bg-gray-200 border border-gray-300 dark:border-gray-700 hover:shadow-md"
@@ -13,7 +13,7 @@ const CoursesGrid = ({ data }) => {
             </span>
           </div>
         );
-      })}
+      }) */}
     </div>
   );
 };
--- pages/subject/physics.js
+++ pages/subject/physics.js
@@ -12,13 +12,13 @@ const Courses = ({ courses }) => {
     return (
         <div>
             <h1>All the courses</h1>
-            {courses.map(course => (
+            {/* courses.map(course => (
                 <div key={course.[0]}>
                     <a>
                         <h2>{ course.[0] }</h2>
                     </a>
                 </div>
-            ))}
+            )) */}
         </div>
     )
 }
