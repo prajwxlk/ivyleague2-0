b87f216ccdb371605b2a8b908b9f6ea37e023e82
Praj964 <prajwify@gmail.com>
2021-07-16 13:03:26 +0530

    comments out

--- pages/subject/physics.js
+++ pages/subject/physics.js
@@ -12,13 +12,6 @@ const Courses = ({ courses }) => {
     return (
         <div>
             <h1>All the courses</h1>
-            {/* courses.map(course => (
-                <div key={course.[0]}>
-                    <a>
-                        <h2>{ course.[0] }</h2>
-                    </a>
-                </div>
-            )) */}
         </div>
     )
 }
