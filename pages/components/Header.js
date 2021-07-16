b4b5bbc6948e5534ade630f1751dbdfe8d0d158b
Prajwal <73160418+Praj964@users.noreply.github.com>
2021-07-16 11:59:22 +0530

    Update Header.js

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -5,22 +5,22 @@ export default function Header() {
     return (
         <div>
             <header>
-                <div className="px-20 bg-transparent">
-                    <div className="flex flex-row gap-4 p-4 justify-center">
-                        <div>
-                            <Link href="/all">
+                <div className=" bg-transparent">
+                    <div className="flex flex-row grid grid-cols-3 sm:grid-cols-6 lg:grid-cols-8 p-4 justify-center">
+                    <div>
+                            <Link href="/subject/all">
                                 <a>
-                                    <button className="flex items-center px-6 py-2 mt-auto font-semibold text-white bg-blue-600 hover:bg-blue-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
-                                        All
+                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-4 sm:py-2 font-normal sm:font-semibold text-white bg-blue-600 hover:bg-blue-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                        All Subjects
                                     </button>
                                 </a>
                             </Link>
                         </div>
 
                         <div>
-                            <Link href="/chemistry">
+                            <Link href="/subject/chemistry">
                                 <a>
-                                    <button className="flex items-center px-6 py-2 mt-auto font-semibold text-white bg-gradient-to-bl from-green-400 to-blue-500 hover:from-green-600 hover:to-blue-700 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-4 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-green-400 to-blue-500 hover:from-green-600 hover:to-blue-700 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Chemistry
                                     </button>
                                 </a>
@@ -28,9 +28,9 @@ export default function Header() {
                         </div>
 
                         <div>
-                            <Link href="/physics">
+                            <Link href="/subject/physics">
                                 <a>
-                                    <button className="flex items-center px-6 py-2 mt-auto font-semibold text-white bg-gradient-to-bl from-pink-500 to-blue-600 hover:from-pink-700 hover:to-blue-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-6 py-3 mx-1 my-1 sm:px-8 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-pink-500 to-blue-600 hover:from-pink-700 hover:to-blue-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Physics
                                     </button>
                                 </a>
@@ -38,9 +38,9 @@ export default function Header() {
                         </div>
 
                         <div>
-                            <Link href="/maths">
+                            <Link href="/subject/maths">
                                 <a>
-                                    <button className="flex items-center px-6 py-2 mt-auto font-semibold text-white bg-gradient-to-bl from-green-400 to-yellow-400 hover:from-green-600 hover:to-yellow-600 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-8 py-3 mx-1 my-1 sm:px-8 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-green-400 to-yellow-400 hover:from-green-600 hover:to-yellow-600 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Maths
                                     </button>
                                 </a>
@@ -48,9 +48,9 @@ export default function Header() {
                         </div>
 
                         <div>
-                            <Link href="/biology">
+                            <Link href="/subject/biology">
                                 <a>
-                                    <button className="flex items-center px-6 py-2 mt-auto font-semibold text-white bg-gradient-to-b from-red-500 to-yellow-600 hover:from-red-700 hover:to-yellow-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-6 py-3 mx-1 my-1 sm:px-8 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-b from-red-500 to-yellow-600 hover:from-red-700 hover:to-yellow-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Biology
                                     </button>
                                 </a>
@@ -58,9 +58,9 @@ export default function Header() {
                         </div>
 
                         <div>
-                            <Link href="/philosophy">
+                            <Link href="/subject/philosophy">
                                 <a>
-                                    <button className="flex items-center px-6 py-2 mt-auto font-semibold text-white bg-gradient-to-bl from-pink-400 via-red-400 to-red-600 hover:from-pink-600 hover:via-red-600 hover:to-red-600 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-pink-400 via-red-400 to-red-600 hover:from-pink-600 hover:via-red-600 hover:to-red-600 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Philosophy
                                     </button>
                                 </a>
@@ -68,9 +68,9 @@ export default function Header() {
                         </div>
 
                         <div>
-                            <Link href="/psychology">
+                            <Link href="/subject/psychology">
                                 <a>
-                                    <button className="flex items-center px-6 py-2 mt-auto font-semibold text-white bg-gradient-to-tr from-yellow-500 to-pink-600 hover:from-yellow-700 hover:to-pink-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-tr from-yellow-500 to-pink-600 hover:from-yellow-700 hover:to-pink-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Psychology
                                     </button>
                                 </a>
@@ -78,9 +78,9 @@ export default function Header() {
                         </div>
 
                         <div>
-                            <Link href="/computers">
+                            <Link href="/subject/computers">
                                 <a>
-                                    <button className="flex items-center px-6 py-2 mt-auto font-semibold text-white bg-gradient-to-bl from-red-400 to-yellow-500 hover:from-red-500 hover:to-yellow-600 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-red-400 to-yellow-500 hover:from-red-500 hover:to-yellow-600 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Computers
                                     </button>
                                 </a>
