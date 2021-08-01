05f9b10560bab6c0b5fed672e52d988b880e4f01
Praj964 <prajwify@gmail.com>
2021-08-01 18:09:24 +0530

    removed clickable divs and added btn

--- pages/index.js
+++ pages/index.js
@@ -32,9 +32,7 @@ export default function Home() {
         <section  className="flex flex-col sm:flex-row gap-4 pb-8 mx-4">
           <cards>
             <div className="container">
-                <Link href="/subject/all">
-                  <a>
-                    <div className="p-2 px-2 py-10 sm:px-5 sm:py-40 rounded-lg shadow-xl bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-600 hover:to-blue-700">
+                    <div className="p-2 px-2 py-10 sm:px-5 sm:py-40 rounded-lg shadow-xl bg-gradient-to-r from-green-400 to-blue-500">
                       <div className="flex-grow p-6 py-2 rounded-lg">
                         <p className="mb-2 text-2xl font-bold leading-none tracking-tighter text-white sm:text-5xl"> 
                           Jack of all trades?
@@ -42,17 +40,20 @@ export default function Home() {
                         <p className="mb-3 text-xl font-semibold leading-relaxed text-gray-100	"> 
                           All the courses from top universities like Harvard, Stanford, MIT, etc for Free!
                         </p>
+                        <Link href="/subject/all">
+                                    <a target="_blank">
+                                        <button className="px-8 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900 "> 
+                                            View Courses
+                                        </button>
+                                    </a>    
+                                </Link>
                       </div>
                     </div>
-                  </a>
-                </Link>
             </div>
           </cards>
           <grid className="grid grid-cols-1 sm:grid-cols-2 gap-4">
               <div>
-                <Link href="/subject/maths">
-                  <a>
-                    <div className="p-2 px-5 py-10 bg-gradient-to-tl from-green-400 to-yellow-400 hover:from-green-600 hover:to-yellow-600 rounded-lg shadow-xl">
+                    <div className="p-2 px-5 py-10 bg-gradient-to-tl from-green-400 to-yellow-400 rounded-lg shadow-xl">
                       <div className="flex-grow p-6 py-2 rounded-lg">
                         <p className="mb-2 text-2xl font-bold leading-none tracking-tighter text-white sm:text-3xl"> 
                           Numbers say more than words?
@@ -60,16 +61,19 @@ export default function Home() {
                         <p className="mb-3 text-xl font-semibold leading-relaxed text-gray-100	"> 
                           Courses on Mathematics from MIT & Harvard.
                         </p>
+                        <Link href="/subject/maths">
+                                    <a target="_blank">
+                                        <button className=" px-8 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900 "> 
+                                            View Courses
+                                        </button>
+                                    </a>    
+                                </Link>
                       </div>
                     </div>
-                  </a>
-                </Link>
               </div>
 
               <div>
-                <Link href="/subject/physics">
-                  <a>
-                    <div className="p-2 px-5 py-10 bg-gradient-to-bl from-pink-500 to-blue-600 hover:from-pink-700 hover:to-blue-800 rounded-lg shadow-xl">
+                    <div className="p-2 px-5 py-10 bg-gradient-to-bl from-pink-500 to-blue-600 rounded-lg shadow-xl">
                       <div className="flex-grow p-6 py-2 rounded-lg">
                         <p className="mb-2 text-2xl font-bold leading-none tracking-tighter text-white sm:text-3xl"> 
                           Fan of Feynman?
@@ -77,16 +81,19 @@ export default function Home() {
                         <p className="mb-3 text-xl font-semibold leading-relaxed text-gray-100	"> 
                           Courses on Physics, Biology, Chemistry from MIT, Yale & Stanford.
                         </p>
+                        <Link href="/subject/physics">
+                                    <a target="_blank">
+                                        <button className=" px-8 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900 "> 
+                                            View Courses
+                                        </button>
+                                    </a>    
+                                </Link>
                       </div>
                     </div>
-                  </a>
-                </Link>
               </div>
 
               <div>
-                <Link href="/subject/computers">
-                  <a>
-                    <div className="p-2 px-5 py-10 bg-gradient-to-r from-red-400 to-yellow-500 hover:from-red-500 hover:to-yellow-600 rounded-lg shadow-xl">
+                <div className="p-2 px-5 py-10 bg-gradient-to-r from-red-400 to-yellow-500 rounded-lg shadow-xl">
                       <div className="flex-grow p-6 py-2 rounded-lg">
                         <p className="mb-2 text-2xl font-bold leading-none tracking-tighter text-white sm:text-3xl"> 
                           Think like Bill?
@@ -94,16 +101,19 @@ export default function Home() {
                         <p className="mb-3 text-xl font-semibold leading-relaxed text-gray-100	"> 
                           Courses on Computers and Machine Learning from MIT & Harvard.
                         </p>
+                        <Link href="/subject/computers">
+                                    <a target="_blank">
+                                        <button className=" px-8 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900 "> 
+                                            View Courses
+                                        </button>
+                                    </a>    
+                                </Link>
                       </div>
                     </div>
-                  </a>
-                </Link>
               </div>
 
               <div>
-                <Link href="/subject/psychology">
-                  <a>
-                    <div className="p-2 px-5 py-10 bg-gradient-to-b from-pink-400 via-red-400 to-red-600 hover:from-pink-600 hover:via-red-600 hover:to-red-600 rounded-lg shadow-xl">
+                <div className="p-2 px-5 py-10 bg-gradient-to-b from-pink-400 via-red-400 to-red-600 rounded-lg shadow-xl">
                       <div className="flex-grow p-6 py-2 rounded-lg">
                         <p className="mb-2 text-2xl font-bold leading-none tracking-tighter text-white sm:text-3xl"> 
                           Quoting Naval often?
@@ -111,10 +121,15 @@ export default function Home() {
                         <p className="mb-3 text-xl font-semibold leading-relaxed text-gray-100	"> 
                           Courses on Psychology and Philosophy from Yale and University of Toronto.
                         </p>
+                        <Link href="/subject/philosophy">
+                                    <a target="_blank">
+                                        <button className=" px-8 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900 "> 
+                                            View Courses
+                                        </button>
+                                    </a>    
+                                </Link>
                       </div>
                     </div>
-                  </a>
-                </Link>
               </div>
           </grid>
         </section>
