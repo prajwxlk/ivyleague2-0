bf3c210144b60958e33b0d283016f993d01e8c40
Praj964 <prajwify@gmail.com>
2021-08-01 17:49:22 +0530

    navigation changed to simple white bg

--- pages/components/Navigation.js
+++ pages/components/Navigation.js
@@ -10,7 +10,7 @@ export default function Navigation() {
                     <div>
                             <Link href="/subject/all">
                                 <a>
-                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-4 sm:py-2 font-normal sm:font-semibold text-white bg-blue-600 hover:bg-blue-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-4 sm:py-2 font-normal sm:font-semibold text-black bg-white hover:bg-gray-400 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         All Subjects
                                     </button>
                                 </a>
@@ -20,7 +20,7 @@ export default function Navigation() {
                         <div>
                             <Link href="/subject/chemistry">
                                 <a>
-                                    <button className="items-center px-4 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-green-400 to-blue-500 hover:from-green-600 hover:to-blue-700 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-4 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-semibold text-black bg-white hover:bg-gray-400	transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Chemistry
                                     </button>
                                 </a>
@@ -30,7 +30,7 @@ export default function Navigation() {
                         <div>
                             <Link href="/subject/physics">
                                 <a>
-                                    <button className="items-center px-6 py-3 mx-1 my-1 sm:px-8 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-pink-500 to-blue-600 hover:from-pink-700 hover:to-blue-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-6 py-3 mx-1 my-1 sm:px-8 sm:py-2 font-normal sm:font-semibold text-black bg-white hover:bg-gray-400 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Physics
                                     </button>
                                 </a>
@@ -40,7 +40,7 @@ export default function Navigation() {
                         <div>
                             <Link href="/subject/maths">
                                 <a>
-                                    <button className="items-center px-8 py-3 mx-1 my-1 sm:px-8 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-green-400 to-yellow-400 hover:from-green-600 hover:to-yellow-600 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-8 py-3 mx-1 my-1 sm:px-8 sm:py-2 font-normal sm:font-semibold text-black bg-white hover:bg-gray-400 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Maths
                                     </button>
                                 </a>
@@ -50,7 +50,7 @@ export default function Navigation() {
                         <div>
                             <Link href="/subject/biology">
                                 <a>
-                                    <button className="items-center px-6 py-3 mx-1 my-1 sm:px-8 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-b from-red-500 to-yellow-600 hover:from-red-700 hover:to-yellow-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-6 py-3 mx-1 my-1 sm:px-8 sm:py-2 font-normal sm:font-semibold text-black bg-white hover:bg-gray-400 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Biology
                                     </button>
                                 </a>
@@ -60,7 +60,7 @@ export default function Navigation() {
                         <div>
                             <Link href="/subject/philosophy">
                                 <a>
-                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-pink-400 via-red-400 to-red-600 hover:from-pink-600 hover:via-red-600 hover:to-red-600 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-black bg-white hover:bg-gray-400 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Philosophy
                                     </button>
                                 </a>
@@ -70,7 +70,7 @@ export default function Navigation() {
                         <div>
                             <Link href="/subject/psychology">
                                 <a>
-                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-tr from-yellow-500 to-pink-600 hover:from-yellow-700 hover:to-pink-800 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-black bg-white hover:bg-gray-400 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Psychology
                                     </button>
                                 </a>
@@ -80,7 +80,7 @@ export default function Navigation() {
                         <div>
                             <Link href="/subject/computers">
                                 <a>
-                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-white bg-gradient-to-bl from-red-400 to-yellow-500 hover:from-red-500 hover:to-yellow-600 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                                    <button className="items-center px-3 py-3 mx-1 my-1 sm:px-6 sm:py-2 font-normal sm:font-semibold text-black bg-white hover:bg-gray-400 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
                                         Computers
                                     </button>
                                 </a>
