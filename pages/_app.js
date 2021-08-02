ae5f4bdc21e8836f2006eb4bd0e227ce13a14a89
Praj964 <prajwify@gmail.com>
2021-08-02 07:51:21 +0530

    splitbee installed

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import 'tailwindcss/tailwind.css'
+import splitbee from '@splitbee/web';
+
+// This initiliazes Splitbee.js
+splitbee.init()
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
