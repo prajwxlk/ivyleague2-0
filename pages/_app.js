13c2cf528c033f7baccb9772cad5dc7d2181a0fa
Prajwal <73160418+Praj964@users.noreply.github.com>
2021-08-02 11:06:01 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,31 +8,24 @@ splitbee.init()
 function MyApp({ Component, pageProps }) {
   return 
   <div>
-    <Head><!-- Primary Meta Tags -->
-      <title>Internet Is Awesome - Learn from the top universities
-      in the world for free!</title>
-      <meta name="title" content="Internet Is Awesome - Learn from the top universities
-      in the world for free!">
-      <meta name="description" content="Learn from the top universities
-      in the world for free!">
+    <Head>
+      
+      <title>Learn from the top universities for Free</title>
+      <meta name="description" content="">
 
-      <!-- Open Graph / Facebook -->
+      <meta property="og:url" content="https://www.internetisawesome.xyz/">
       <meta property="og:type" content="website">
-      <meta property="og:url" content="https://metatags.io/">
-      <meta property="og:title" content="Internet Is Awesome - Learn from the top universities
-      in the world for free!">
-      <meta property="og:description" content="Learn from the top universities
-      in the world for free!">
-      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+      <meta property="og:title" content="Learn from the top universities for Free">
+      <meta property="og:description" content="">
+      <meta property="og:image" content="">
 
-      <!-- Twitter -->
-      <meta property="twitter:card" content="summary_large_image">
-      <meta property="twitter:url" content="https://metatags.io/">
-      <meta property="twitter:title" content="Internet Is Awesome - Learn from the top universities
-      in the world for free!">
-      <meta property="twitter:description" content="Learn from the top universities
-      in the world for free!">
-      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+      <meta name="twitter:card" content="summary_large_image">
+      <meta property="twitter:domain" content="internetisawesome.xyz">
+      <meta property="twitter:url" content="https://www.internetisawesome.xyz/">
+      <meta name="twitter:title" content="Learn from the top universities for Free">
+      <meta name="twitter:description" content="">
+      <meta name="twitter:image" content="">
+
   </Head>
   <Component {...pageProps} />
   </div>
