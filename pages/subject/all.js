ad857fb30f8204cb24de34a438b6d8f0c171fcac
Prajwal <73160418+Praj964@users.noreply.github.com>
2021-09-07 11:04:32 +0530

    mini mba  update page on all

--- pages/subject/all.js
+++ pages/subject/all.js
@@ -204,7 +204,23 @@ export default function Index() {
                         </div>
                     </div>
 
-
+                    <div className="container items-center  text-blueGray-500">
+                        <div className="p-2 my-2 sm:my-6 bg-white border rounded-lg shadow-xl">
+                          <div className="p-6">
+                                <h4 className="mb-2 text-2xl text-center font-semibold leading-none tracking-tighter text-black lg:text-3xl title-font "> 
+                                  Mini MBA
+                                </h4>
+                                <Link href="/subject/mini-mba">
+                                    <a>
+                                        <button className="w-full px-8 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900 "> 
+                                            View Courses
+                                        </button>
+                                    </a>    
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </body>
         </div>
