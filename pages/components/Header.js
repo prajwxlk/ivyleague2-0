11a96f6dc1caaf019fe69525b77d2b3aa40d633e
Prajwal <73160418+Praj964@users.noreply.github.com>
2021-09-07 16:40:10 +0530

    button size restored

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -7,9 +7,9 @@ export default function Footer() {
           <div className="container items-center">
             <footer className="text-blueGray-700 transition duration-500 ease-in-out transform bg-transparent">
               <div className="flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row">
-                <span className="inline-flex justify-centermr-2 sm:ml-auto sm:mt-0 sm:justify-start">
+                <span className="inline-flex justify-center mr-2 sm:ml-auto sm:mt-0 sm:justify-start">
                     <button 
-                    className="px-3 sm:px-6 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
+                    className="px-3 py-1 sm:px-6 sm:py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
                     >
                         <Link target="_blank" href="https://www.buymeacoffee.com/internetiscool">
                             <a target="_blank">
